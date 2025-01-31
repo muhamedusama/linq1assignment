6b70d3e50a7c881fb172e968561fed11ab3f74e2
muhamedusama <muhamedusama2001@gmail.com>
2025-01-31 14:20:09 +0200

    Add project files.

---
+++ linq1assignment/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace linq1assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
