c79b3962459705eea92970a8debe198c2380fe44
muhamedusama <muhamedusama2001@gmail.com>
2025-02-03 08:15:57 +0200

    restrication operators

--- linq1assignment/Program.cs
+++ linq1assignment/Program.cs
@@ -1,10 +1,32 @@
-﻿namespace linq1assignment
+﻿using static linq1assignment.ListGenerator;
+namespace linq1assignmen
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Restrication operator
+            #region Q1
+            //var result = ProductList.Where(p => p.UnitsInStock == 0);
+            //foreach (var item in result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region Q2
+            //var result = ProductList.Where(p => p.UnitsInStock > 0 && p.UnitPrice > 3);
+            //foreach (var item in result)
+            //    Console.WriteLine(item);
+            #endregion
+
+            #region Q3
+            //string[] Arr = { "zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine" };
+            //var result = Arr.Where((p, i) => p.Length < i);
+            //foreach (var item in result) 
+            //    Console.WriteLine(item);
+            #endregion
+
+            #endregion
+
         }
     }
 }
